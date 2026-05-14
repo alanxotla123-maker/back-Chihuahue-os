d75c566212c27c0fb6db8ffbfdf0d8538b60569f
alanxotla123 <alanxotla123@gmail.com>
2026-05-14 10:11:29 -0600

    feat: rutas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutesModule } from './routes/routes.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { AuthModule } from './auth/auth.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [],
+  imports: [RoutesModule, TicketsModule, AuthModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
