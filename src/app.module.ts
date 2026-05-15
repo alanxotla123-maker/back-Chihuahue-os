e5e273b88bd0df2184577e4cdf9c9bef885cb95a
alanxotla123 <alanxotla123@gmail.com>
2026-05-14 19:55:28 -0600

    feat: docker creado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RoutesModule } from './routes/routes.module';
-import { TicketsModule } from './tickets/tickets.module';
 import { AuthModule } from './auth/auth.module';
-import { FilesModule } from './files/files.module';
+import { EmployeesModule } from './employees/employees.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [RoutesModule, TicketsModule, AuthModule, FilesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, EmployeesModule, LocationsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
